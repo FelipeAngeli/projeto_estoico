736d8966ed07880787e63b7c2cad43d8b510051e
Felipe Angeli <felipe.angeli@gmail.com>
2024-04-12 16:21:29 -0300

    repo

--- lib/app/data/repository/estoicismo_repoitory.dart
+++ lib/app/data/repository/estoicismo_repoitory.dart
@@ -4,19 +4,47 @@ import 'package:projeto_estoico/app/utils/app_error.dart';
 
 class EstoicismoRepository {
   final EstoicismoProvider provider;
+  List<EstoicismoModel> _cache = [];
 
-  EstoicismoRepository(this.provider);
+  EstoicismoRepository(this.provider) {
+    _initCache();
+  }
 
+  // Inicializa o cache com dados carregados do provider
+  void _initCache() async {
+    _cache = await provider.loadFrasesEstoicas();
+  }
+
+  // Obtém as frases, possivelmente filtradas por uma query
   Future<List<EstoicismoModel>> getFrasesEstoicas({String query = ''}) async {
     try {
-      final frases = await provider.loadFrasesEstoicas();
+      // Se não há query, retorna o cache
       if (query.isEmpty) {
-        return frases;
+        return _cache;
       } else {
-        return frases.where((frase) => frase.frase.toLowerCase().contains(query.toLowerCase())).toList();
+        // Filtra o cache baseado na query e retorna os resultados
+        return _cache.where((frase) => frase.frase.toLowerCase().contains(query.toLowerCase())).toList();
       }
     } catch (e) {
       throw AppError(message: 'Falha ao carregar as frases estoicas', code: ErrorCode.network);
     }
   }
 }
+// class EstoicismoRepository {
+//   final EstoicismoProvider provider;
+
+//   EstoicismoRepository(this.provider);
+
+//   Future<List<EstoicismoModel>> getFrasesEstoicas({String query = ''}) async {
+//     try {
+//       final frases = await provider.loadFrasesEstoicas();
+//       if (query.isEmpty) {
+//         return frases;
+//       } else {
+//         return frases.where((frase) => frase.frase.toLowerCase().contains(query.toLowerCase())).toList();
+//       }
+//     } catch (e) {
+//       throw AppError(message: 'Falha ao carregar as frases estoicas', code: ErrorCode.network);
+//     }
+//   }
+// }
